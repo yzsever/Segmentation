b8cacf9d29a0aa04553a95c50421d4773b6a5a08
yaozhisheng <291820586@qq.com>
2014-05-07 16:39:34 +0800

    the work is done!

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 Segmentation
 ============
+gcc -g segmentation.c -o test
+gdb test
+r
+此时可以看到程序中的错误
+
+1 堆内存访问越界，并且没有释放（fun1）
+2 使用未初始化的内存（main）
+3 内存读写越界 （fun2）
+4 已释放的内存继续使用（fun3）
+5 内存泄漏（badleak）
+
+patch -p0 segmentation.c < seg.patch
+
+然后就可以正常运行了
---
+++ segmentation_new.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct _node{
+		struct _node  *l;
+		struct _node  *r;
+		char v;
+}node;
+
+node *mk(node *l,node *r,char val)
+{
+	node *f=(node *)malloc(sizeof(*f));
+	f->l=l;
+	f->r=r;
+	f->v=val;
+	return f;
+}
+
+void nodefr(node *n)
+{
+	if(n)
+	{
+		nodefr(n->l);
+		nodefr(n->r);
+		free(n);
+	}
+}
+void fun1()
+{
+	int *p=(int *)malloc(10*sizeof(int));
+	p[9]=0;
+	free(p);
+}
+
+int fun2()
+{
+	int len=4,i;
+	int *pt=(int *)malloc(len*sizeof(int));
+	int *p=pt;
+
+	for(i=0;i<len;i++)
+	{
+		p++;
+	}
+
+	p--;
+	*p=5;
+	printf("the value of p equal:%d",*p);
+	free(pt);
+	return 0;
+}
+
+
+
+int badleak()
+{
+	node *tree1,*tree2,*tree3;
+	tree1=mk(mk(mk(0,0,'3'),0,'2'),0,'1');
+	tree2=mk(0,mk(0,mk(0,0,'6'),'5'),'4');
+	tree3=mk(mk(tree1,tree2,'8'),0,'7');
+	nodefr(tree3);
+	return 0;
+}
+
+int fun3()
+{
+	char *p=NULL;
+	p=(char *)malloc(sizeof(char));
+        *p='s';
+	free(p);
+	return 0;	
+}
+
+int main(int argc,char * argv[])
+{
+   	int a[5];
+	int i,s;
+	char my_string[]="hello there";
+	a[0]=a[1]=a[2]=a[3]=a[4]=0;
+	s=0;
+	for(i=0;i<5;i++)
+			s+=a[i];
+	if(s==377)
+			printf("sum is %d\n",s);
+	fun1();
+	fun2();
+	fun3();
+	badleak();
+	return 0;
+}
